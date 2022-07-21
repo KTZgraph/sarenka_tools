5a9020d48431abf9e4a15fdcb3e3f5445f403d4e
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-07-21 23:45:42 +0200

    Update utils.py

--- joomla_version_scrapper/utils.py
+++ joomla_version_scrapper/utils.py
@@ -2,7 +2,7 @@ import json
 import requests
 import zipfile
 import os
-from typing import OrderedDict, Optional
+from typing import Optional
 
 
 def save_dict_as_json(data: dict, filename: str) -> None:
@@ -16,12 +16,12 @@ def get_dict_from_json_file(filepath: str) -> dict:
     return data
 
 
-def download_zip_file(URL: str, filename: str) -> str:
+def download_zip_file(URL: str, dst_filepath: str) -> str:
     data = requests.get(URL)
-    with open(filename, "wb") as f:
+    with open(dst_filepath, "wb") as f:
         f.write(data.content)
 
-    return filename
+    return dst_filepath
 
 
 def unzip_package(zip_filename: str, extract_destination="./") -> list[Optional[str]]:
@@ -39,3 +39,28 @@ def get_files_names_from_dir(dirpath: str) -> list[str]:
 def save_zip_file(source_filepath: str, output_filepath: str) -> None:
     with zipfile.ZipFile(output_filepath, "w") as zip:
         zip.write(source_filepath)
+
+
+def create_dir_if_not_exists(dir_path: str):
+    if not os.path.isdir(dir_path):
+        os.mkdir(dir_path)
+
+
+def get_folder_structure(filepath: str) -> list[str]:
+    """zwraca po kolei foldery do pliku 
+    - całą struktura katalogów potrzerbna do stworzenia drzewka folderów"""
+    folders_list = filepath.split("/")
+    structure = []
+    for deph in range(len(folders_list)):
+        tmp_path = [folders_list[i] for i in range(deph)]
+        tmp_path = ("/").join(tmp_path)
+        if tmp_path != "":
+            structure.append(tmp_path)
+
+    return structure
+
+
+def create_folder_structure(structure: list[str]) -> None:
+    """Tworzy strukturę folderów na podstawie listy plików """
+    for dirpath in structure:
+        create_dir_if_not_exists(dirpath)
